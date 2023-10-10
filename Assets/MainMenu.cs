1dcfb419c84ed84bd21ae9a3b3cc8c91b996a4a1
Aliisdum <alinabdulla@icloud.com>
2023-10-10 18:23:19 +0400

    v3 Huge Update
    Added A new Car, New menu And new Map

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public void Play11()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Car1 Game1");
     }
-    public void Pause()
+     public void Play12()
     {
-        SceneManager.LoadScene("Pause");
+        SceneManager.LoadScene("Car1Game2");
     }
-         
-       
+      public void Play21()
+    {
+        SceneManager.LoadScene("Car2Game1");
+    }   
+    
+        public void Play()
+    {
+        SceneManager.LoadScene("Map");
+    }
+      public void Play22()
+    {
+        SceneManager.LoadScene("Car2 Game2");
+    }
 }
